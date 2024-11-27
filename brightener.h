4ff64885d5d65e84e8d5bc803ac6bb130935ccc9
Praveen Kumar Tiwari <PraveenKumar.Tiwari@philips.com>
2024-11-27 14:00:01 +0530

    refactore and reduce code is done

--- brightener.h
+++ brightener.h
@@ -1,14 +1,20 @@
 #pragma once
 
-#include <cstdint>
 #include <memory>
 #include "./image.h"
 
 class ImageBrightener {
  private:
     std::shared_ptr<Image> m_inputImage;
+
  public:
     explicit ImageBrightener(std::shared_ptr<Image> inputImage);
+
+    // Brighten the entire image
     int BrightenWholeImage();
-    bool AddBrighteningImage(std::shared_ptr<Image> imageToAdd, int& attenuatedCount);
+
+    // Add the pixel values of another image to the current image
+    bool AddBrighteningImage(std::shared_ptr<Image> imageToAdd, int* attenuatedCount);
+    // Helper function
+    bool AreImagesCompatible(std::shared_ptr<Image> imageToAdd);
 };
--- image.h
+++ image.h
@@ -1,20 +1,16 @@
 #pragma once
+
 #include <cstdint>
-#include <iostream>
+#include <vector>
+#include <functional>
 
 class Image {
  public:
-    Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
-        pixels = new uint8_t[m_rows * m_columns];
-    }
-    ~Image() {
-        std::cout << "freeing pixel memory.\n";
-        delete[] pixels;
-    }
-    bool imageSizeIsValid() {
-        return m_rows <= 1024 && m_columns <= 1024;
-    }
-    const uint16_t m_rows;
-    const uint16_t m_columns;
-    uint8_t* pixels;  // max 1k x 1k image
+    int m_rows, m_columns;
+    std::vector<uint8_t> pixels;  // A vector of (grayscale) pixels
+
+    Image(int rows, int columns) : m_rows(rows), m_columns(columns), pixels(rows * columns) {}
+
+    // Method to process each pixel in the image
+    void Image::processPixels(const std::function<void(int, int, uint8_t&)>& processor);
 };
